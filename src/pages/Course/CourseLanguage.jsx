186076d19ceeca501561869ba91266a5d3756798
@sahil2k19 <51903665+sahil2k19@users.noreply.github.com>
2024-04-28 21:47:45 +0530

    add progress

--- src/pages/Course/CourseLanguage.jsx
+++ src/pages/Course/CourseLanguage.jsx
@@ -1,14 +1,100 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import {LinearProgress} from '@mui/material'
+import {Button, LinearProgress} from '@mui/material'
 const CourseLanguage = () => {
     const {language} = useParams()
-    const [progressValue, setProgressValue] = useState(78)
-
+    const [progressValue, setProgressValue] = useState(0)
+    const [step, setStep] = useState(1);
     const youtubeVideoUrl = `https://www.youtube.com/watch?v=bEBNel06JJM`
     const videoId = youtubeVideoUrl.split('v=')[1].split('&')[0];
     const iframeSrc = `https://www.youtube.com/embed/${videoId}`;
     
+    const handleSetProgress = () => {
+        console.log('step', step)
+        switch(step){
+            case 1:
+                setProgressValue(0)
+                break;
+            case 2:
+                setProgressValue(50)
+                break;
+            case 3:
+                setProgressValue(75)
+                break;
+            default:
+                setProgressValue(0)
+        }
+    }
+    useEffect(()=>{
+        handleSetProgress()
+    },[step])
+
+    const handleNext = () => {
+        console.log('step', step)
+
+        if(step<2){
+            setStep(prev=>prev + 1);
+            
+        }
+        // handleSetProgress()
+    }
+
+    const handlePrev = () => {
+        console.log('step', step)
+
+        if(step>1){
+            setStep(prev=>prev - 1);
+        }
+        // handleSetProgress()
+    }
+
+    const CourseVideo = () => {
+
+        return (
+            <>
+             <div className='d-flex justify-content-center mt-5'>
+                    <iframe
+                            width="560"
+                            height="315"
+                            src={iframeSrc}
+                            title="YouTube video player"
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        ></iframe>
+                    </div>
+            
+            </>
+        )
+    }
+
+    const CourseQuiz = () => {
+
+        return (
+            <>
+             <div className='d-flex justify-content-center mt-5'>
+                    <div>
+                        <h1>Quiz</h1>
+                        <p>Coming Soon</p>  
+
+                    </div>
+                    </div>
+            
+            </>
+        )
+    }
+
+    const showContent = () => {
+        switch(step){
+            case 1:
+                return <CourseVideo/>
+            case 2:
+                return <CourseQuiz/>
+           
+            default:
+                return <CourseVideo/>
+        }
+    }
 
   return (
     <>
@@ -24,23 +110,21 @@ const CourseLanguage = () => {
                 <div className='d-flex justify-content-center mt-3'>
                 <LinearProgress className='w-75' variant="determinate" value={progressValue} />
                 </div>
-
-                <div className='d-flex justify-content-center mt-5'>
-                <iframe
-                        width="560"
-                        height="315"
-                        src={iframeSrc}
-                        title="YouTube video player"
-                        frameBorder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen
-                    ></iframe>
-                </div>
+                {showContent()}
+               <div className='d-flex justify-content-between'>
+                <Button variant="contained" onClick={handlePrev} className='mt-5'>Prev</Button>
+                <Button variant="contained" onClick={handleNext} className='mt-5'>Next</Button>
+               </div>
             </div>
 
         </div>
+
     </>
   )
 }
+
+
+
+
 
 export default CourseLanguage
